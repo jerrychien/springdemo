9bf4bcfa74745c6844776b749c69002a20c63891
jerrychien <qianzhiyong111@163.com>
2016-09-13 23:12:32 +0800

    propertiesUtil工具类

---
+++ src/main/java/com/jerry/constant/Constants.java
@@ -0,0 +1,29 @@
+package com.jerry.constant;
+
+import com.jerry.util.PropertiesUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Constants
+ *
+ * @author jerrychien
+ * @create 2016-09-13 23:06
+ */
+public final class Constants {
+
+    private static final Logger logger = LoggerFactory.getLogger(Constants.class);
+
+    private static final String jdbcDriver;
+
+    private static final String jdbcUrl;
+
+    private static final PropertiesUtil jdbcProperties = PropertiesUtil.newInstance("/jdbc.properties");
+
+    static {
+        jdbcDriver = jdbcProperties.getProperty("jdbc.driver", "JDBC_DRIVER");
+        jdbcUrl = jdbcProperties.getProperty("jdbc.url", "JDBC_URL");
+        logger.info("jdbcDriver:{},jdbcUrl:{}", jdbcDriver, jdbcUrl);
+    }
+
+}
